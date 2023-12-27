d45a015d494e7904d7850f75f09583b2f28e8513
7juned7 <96330909+7juned7@users.noreply.github.com>
2023-12-27 18:28:22 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const dotenv = require("dotenv");
+require("dotenv").config()
 const userRoutes = require("./Routes/userRoutes")
 const studentRoutes = require("./Routes/studentRoutes")
 const connectDB = require("./config/db");
 const path = require("path");
-dotenv.config()
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 4000;
 connectDB()
 
 const app = express();
@@ -18,16 +18,20 @@ app.use('/api/user', userRoutes)
 app.use('/api/studentdata', studentRoutes)
 
 // --------deployement-----------
-
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV == "development") {
-    app.use(express.static(path.join(__dirname1, "/frontend/build", "index.html")));
+const dev = process.env.NODE_ENV
+if (dev === "development") {
+
+    app.use(express.static(path.join(__dirname1, "/frontend/build")));
     app.get('*', (req, res) => {
-        res.send("API is running")
+
+
+        res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
     });
 } else {
     app.get('/', (req, res) => {
-        res.send("Api is running");
+
+        res.send(dev);
 
     })
 }
