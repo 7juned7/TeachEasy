161638e218f0f62f32942a12320d85425615b779
7juned7 <96330909+7juned7@users.noreply.github.com>
2024-06-28 19:10:26 +0530

    Update TeachEasy.js

--- frontend/src/pages/TeachEasy.js
+++ frontend/src/pages/TeachEasy.js
@@ -2,8 +2,10 @@ import { Box } from '@chakra-ui/react';
 import React from 'react'
 import Allstudents from '../components/Allstudents';
 import Navbar from '../components/Navbar';
+
 import SideDrawer from '../components/SideDrawer';
 import StudentsDetails from '../components/StudentsDetails';
+
 
 
 
@@ -12,13 +14,13 @@ const TeachEasy = () => {
 
     return (<Box
         paddingTop={2}
-        paddingRight={10}
-        paddingLeft={10}
 
+        bg={"#402E7A"}
+        height={"100vh"}
     >
 
         <div><Navbar /></div>
-        <div>
+        <Box padding={" 0 2rem"}>
             <Box >
                 <SideDrawer />
             </Box>
@@ -28,7 +30,8 @@ const TeachEasy = () => {
 
                 <StudentsDetails />
             </Box>
-        </div>
+        </Box>
+
     </Box>
 
     )
