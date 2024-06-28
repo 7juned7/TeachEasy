fb1165fd4f99f4dd9105dbc0e256577384007eeb
7juned7 <96330909+7juned7@users.noreply.github.com>
2024-06-28 19:08:01 +0530

    Update StudentsDetails.js

--- frontend/src/components/StudentsDetails.js
+++ frontend/src/components/StudentsDetails.js
@@ -1,28 +1,39 @@
 import { Avatar, Box, Text } from '@chakra-ui/react'
-import React, { useContext} from 'react'
+import { useContext } from 'react';
+
+
+
 import StudentDataContext from '../Context/StudentDataContext'
+import { Chart as ChartJS, DoughnutController } from 'chart.js/auto'
+import { Bar, Doughnut } from 'react-chartjs-2';
 
 const StudentsDetails = () => {
-    const { particulardata} = useContext(StudentDataContext);
+    const { particulardata } = useContext(StudentDataContext);
     console.log(particulardata)
+
 
 
     return (<Box
         width={"100%"}
+        color={"white"}
+        fontFamily={"sans-serif"}
     >
         {!particulardata ? (
             <div>Hi</div>
-        ) : (<div>
-            <Box border={"2px solid red"}
+        ) : (<Box width={"100%"}
+
+            overflow={"hidden"}
+            height={"100%"}>
+            <Box
                 borderRadius={"lg"}
                 width={"100%"}
                 display={"flex"}
                 justifyContent={"space-between"}
-                p={"2"}>
+                p={"4"}>
                 <Box
                 >
-                    <Text>{particulardata.name}</Text>
-                    <Text>{particulardata.rollno}</Text>
+                    <Text>Name:{particulardata.name}</Text>
+                    <Text>Rollno:{particulardata.rollno}</Text>
                 </Box>
                 <Box>
                     <Avatar
@@ -34,6 +45,7 @@ const StudentsDetails = () => {
                 </Box>
             </Box>
             <Box>
+
 
                 <Box display={"flex"}
                     justifyContent={"space-around"}>
@@ -53,10 +65,68 @@ const StudentsDetails = () => {
                     <Text>{particulardata.Science}</Text>
 
                 </Box>
+                <Box
+                    display={"flex"}
+                    justifyContent={"center"}
+                    alignContent={"center"}
+                    gap={"2rem"}
+                    width={"400px"}
+                    height={"200px"}
+
+                    margin={" 2rem auto"}>
+                    <Bar
+                        data={{
+
+                            labels: ["English", "Maths", "Science"],
+
+                            datasets: [
+                                {
+                                    label: "Total Marks",
+                                    data: [100, 100, 100],
+                                    backgroundColor: ["#3DC2EC"]
+                                },
+                                {
+                                    label: "Marks",
+                                    data: [particulardata.Science, particulardata.Maths, particulardata.English],
+                                    backgroundColor: ["lightGreen"]
+                                },
+
+
+                            ],
+                        }
+                        }
+
+
+                    />
+                    <Doughnut
+
+                        data={{
+
+                            labels: ["Science", "Maths", "English"],
+
+                            datasets: [
+                                {
+
+                                    data: [particulardata.Science, particulardata.Maths, particulardata.English],
+                                    backgroundColor: ["red", "lightGreen", "skyblue"]
+                                },
+
+
+
+                            ],
+                        }
+                        }
+
+
+                    />
+
+                </Box>
+
 
 
             </Box>
-        </div>)}
+
+        </Box>)}
 
 
     </Box>
